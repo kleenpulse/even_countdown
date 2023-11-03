1da5173b8e9cef44d467879803474f2c05678493
Vxrcel <skyeparagon@gmail.com>
2023-11-03 10:58:18 +0100

    chore: meta data

--- components/NumberRotation.tsx
+++ components/NumberRotation.tsx
@@ -13,7 +13,7 @@ const NumberRotation = ({ number }: { number: number }) => {
 	};
 
 	return (
-		<div className="relative h-16 w-16 sm:h-24 sm:w-24 lg:h-40 lg:w-40 flex justify-center max-[333px]:h-20 max-[333px]:w-20">
+		<div className="relative h-16 w-16 sm:h-24 sm:w-24 lg:h-40 lg:w-40 flex justify-center max-[333px]:h-20 max-[333px]:w-20 ">
 			{numbers.map((num) => (
 				<div
 					key={num}
